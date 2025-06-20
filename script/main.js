ece39f5e258806bf59bd1f4f7e0445a0f8c16364
ElliBathory <53011678+ElliBathory@users.noreply.github.com>
2025-06-20 18:15:03 +0200

    casestudy images, header, structure

--- script/main.js
+++ script/main.js
@@ -2,7 +2,10 @@
 let btn_scrollToTop = document.getElementById("scrollToTop");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {toggleScrollToTop()};
+window.onscroll = function () {
+  toggleScrollToTop();
+  toggleStickyHeader();
+};
 
 function toggleScrollToTop() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -13,6 +16,17 @@ function toggleScrollToTop() {
 }
 
 function scrollToTop() {
-    window.scrollTo(0,0);
-    toggleScrollToTop();
+  window.scrollTo(0, 0);
+  toggleScrollToTop();
+}
+
+var header = document.getElementById("casestudy-header");
+var sticky = header.offsetTop;
+
+function toggleStickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
